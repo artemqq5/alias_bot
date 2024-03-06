09d0e6a011eb2624ad3fdfdf3e445615289cdd00
artem <artemqq5@gmail.com>
2024-03-06 17:03:01 +0200

    Update users_.py

--- data/repopsitory/users_.py
+++ data/repopsitory/users_.py
@@ -25,4 +25,4 @@ class UserRepository(DefaultDataBase):
 
     def _update_user_access(self, user_id, access):
         COMMAND_ = '''update `users` set `role` = %s where `user_id` = %s;'''
-        return self._update(COMMAND_, (user_id, access))
+        return self._update(COMMAND_, (access, user_id))
